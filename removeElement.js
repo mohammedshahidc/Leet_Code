7f8711189a05c3ed41ed5823b581a05cd463eb9d
shahid <ms4811879@gmail.com>
2025-04-22 10:30:40 +0530

    removeElement

---
+++ removeElement.js
@@ -0,0 +1,12 @@
+var removeElement = function(nums, val) {
+    let count=0
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==val){
+            nums[count]=nums[i]
+            count++
+        }
+        
+    }
+    return count
+ };
+ console.log(removeElement([0,1,2,2,3,0,4,2],2));
