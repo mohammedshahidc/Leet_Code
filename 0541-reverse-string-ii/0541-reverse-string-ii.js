8cf55e522c50c5667fc8530606d6f6dab719c980
Mohammed Shahid C <ms4811879@gmail.com>
2025-05-28 10:35:15 +0530

    Time: 3 ms (74.17%), Space: 56.4 MB (66.22%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+    for (let i = 0; i < s.length; i = i + 2 * k)
+        s = s.slice(0, i) + s.substring(i, i + k).split('').reverse().join('') + s.slice(i + k);
+    return s;
+};
