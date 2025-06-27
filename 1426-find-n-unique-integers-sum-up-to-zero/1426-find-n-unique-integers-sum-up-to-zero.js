f277511c07e27d17cf69200464d34c8695d0d4e0
Mohammed Shahid C <ms4811879@gmail.com>
2025-06-27 10:00:59 +0530

    Time: 1 ms (34.16%), Space: 54.9 MB (55.28%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+     let res=[]
+    let sum=0
+    for(let i=1;i<n;i++){
+        res.push(i)
+        sum+=i
+        
+    }
+    res.push(-sum)
+    return res
+};
