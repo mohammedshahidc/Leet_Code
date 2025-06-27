2b228fd3bcb1b1b749478ad29c373a1ccfda2c3c
Mohammed Shahid C <ms4811879@gmail.com>
2025-06-27 09:42:56 +0530

    Time: 6 ms (19.44%), Space: 55.3 MB (27.05%) - LeetHub

---
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let res=0
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            let temp=(nums[i]-1)*(nums[j]-1)
+            if(res<temp){
+                res=temp
+            }
+        }
+    }
+    return res
+};
