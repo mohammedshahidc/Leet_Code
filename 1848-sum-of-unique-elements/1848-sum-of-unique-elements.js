026fc93d209e2086c5686d1cd27e5a61d3cf10d2
Mohammed Shahid C <ms4811879@gmail.com>
2025-08-04 14:22:52 +0530

    Time: 1 ms (54.17%), Space: 53.6 MB (63.33%) - LeetHub

---
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function (nums) {
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
+            sum += nums[i]
+        }
+    }
+    return sum
+};
